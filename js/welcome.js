b34184f29f56662a8ccdf4974ffb153b81dd228d
moorthy <moorthy.coder@gmail.com>
2026-08-26 21:59:34 +0530

    welcome page added

---
+++ js/welcome.js
@@ -0,0 +1,13 @@
+﻿(function () {
+  const enter_btn_element = document.querySelector('.enter-btn');
+
+  const handleButtonClickFeedback = (event_object) => {
+    enter_btn_element.style.transform = 'scale(0.96)';
+    setTimeout(() => {
+      enter_btn_element.style.transform = '';
+    }, 150);
+    return true;
+  };
+
+  enter_btn_element ? enter_btn_element.addEventListener('click', handleButtonClickFeedback) : null;
+})();
